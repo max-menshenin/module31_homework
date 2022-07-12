54d8b398f5d12cc38a68628a52e2fa2b4910c240
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-07-12 11:39:22 +0300

    done

---
+++ parser.php
@@ -0,0 +1,37 @@
+<?php
+
+class MetaIterator
+{
+    private $patterns = array('~<meta .*>~mui', '~<title>.*<\/title>~mui');
+    private $array = [];
+    public function __construct(string $text)
+    {
+        foreach ($this->patterns as $pattern) {
+            preg_match_all($pattern, $text, $matches);
+            $this->array = array_merge($this->array, $matches);
+        }
+    }
+    private function recScan($array)
+    {
+        $iterator = new RecursiveArrayIterator($array);
+        while ($iterator->valid()) {
+            if ($iterator->hasChildren()) {
+                $this->recScan(iterator_to_array($iterator->getChildren()));
+            } else {
+                echo htmlspecialchars($iterator->current()) . '<br>';
+            }
+            $iterator->next();
+        }
+    }
+    public function getTagList()
+    {
+        $this->recScan($this->array);
+    }
+}
+$code = file_get_contents('index.html');
+$scan = new MetaIterator($code);
+$result = $scan->getTagList();
+
+
+
+
